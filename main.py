1f0c5df10b04d90001879a7d28f127afe317e43b
Joseph Knutson <101217373+Joey1222@users.noreply.github.com>
2023-07-28 22:07:05 +0000

    ?

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from argparse import ArgumentParser, Namespace
+from pathlib import Path
 
 parser = ArgumentParser()
 parser.add_argument('-v', '--version', action="store_true")
-
+parser.add_argument('path')
+parser.add_argument('output')
 
 args: Namespace = parser.parse_args()
 
+inputdir = Path(args.path)
+outputdir = Path(args.output)
+
+if not inputdir.exists():
+    print("The input directory doesn't exist!")
+    raise SystemExit(1)
+
+if not outputdir.exists():
+    print("The output directory doesn't exist!")
+    raise SystemExit(1)
+
 if args.version:
     print('Version is: 1.0.0')
+else:
+    print(f'Path is: {inputdir}\nOutput is: {outputdir}')
