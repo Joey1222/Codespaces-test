8cf6f755a9f6feb41693831d5f48867b96237520
jmknu <101217373+Joey1222@users.noreply.github.com>
2023-07-29 11:57:25 -0500

    Added a error class

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from argparse import ArgumentParser, Namespace
 from pathlib import Path
 import platform
 
+class InvaildFilePathError(Exception):
+
+    def __init__(self, pathtype):
+        self.pathtype = str(pathtype)
+        print(f'The {self.pathtype} file doesn\'t exist!')
+
 with open('version.txt', 'r', encoding="utf-8") as f:
     file = f.readlines()[0]
 
 parser = ArgumentParser()
-parser.add_argument('-v', '--version', action="version", version=f"Version: {file}, Platform: {platform.system()} {platform.release()} {platform.architecture()[0]} {platform.processor()}")
+parser.add_argument('--version', '-v', action="version", version=f"Version: {file}, Platform: {platform.system()} {platform.release()} {platform.architecture()[0]} {platform.processor()}", help="Shows version and exits")
 parser.add_argument('path')
 parser.add_argument('output')
 
@@ -16,11 +22,9 @@ inputdir = Path(args.path)
 outputdir = Path(args.output)
 
 if not inputdir.exists():
-    print("The input directory doesn't exist!")
-    raise SystemExit(1)
+    raise InvaildFilePathError('input')
 
 if not outputdir.exists():
-    print("The output directory doesn't exist!")
-    raise SystemExit(1)
+    raise InvaildFilePathError('output')
 
 print(f'Path is: {inputdir}\nOutput is: {outputdir}')
