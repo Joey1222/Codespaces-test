a9f19b8be40ca40fe08068cd36b1d84c39fa53c3
jmknu <101217373+Joey1222@users.noreply.github.com>
2023-07-31 11:36:45 -0500

    ?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 import platform
+from PIL import Image
 
 class InvaildFilePathError(Exception):
     def __init__(self, pathtype):
@@ -12,18 +13,18 @@ with open('version.txt', 'r', encoding="utf-8") as f:
 
 parser = ArgumentParser()
 parser.add_argument('--version', '-v', action="version", version=f"Version: {file}, Platform: {platform.system()} {platform.release()} {platform.architecture()[0]} {platform.processor()}", help="Shows version and exits")
+parser.add_argument('mode', choices=["pixelate", "whiteandblack", "noise", "none"], help='Mode to change the image')
 parser.add_argument('path')
 parser.add_argument('output')
+parser.add_argument('--imageformat', choices=["png", "jpeg", "jpg", "gif"], help="Image format for output, vaild choices png, jpeg, jpg, gif")
 
 args: Namespace = parser.parse_args()
-
 inputdir = Path(args.path)
 outputdir = Path(args.output)
+imageformat = args.imageformat
+mode = args.mode
 
 if not inputdir.exists():
     raise InvaildFilePathError('input')
 
-if not outputdir.exists():
-    raise InvaildFilePathError('output')
-
-print(f'Path is: {inputdir}\nOutput is: {outputdir}')
+print(f'Path is: {inputdir}\nOutput is: {outputdir}\nImage format is: {imageformat}\nMode is: {mode}')
